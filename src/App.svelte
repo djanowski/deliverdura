5d362a53d107750a31b1e818a032e4c5f7013548
Damian Janowski <jano@dimaion.com>
2022-01-17 19:25:12 -0300

    Header styling.

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,99 @@
 <script>
+  import { slide } from "svelte/transition";
+
   import Product from "./Product.svelte";
   import Total from "./Total.svelte";
   import OrderDetails from "./OrderDetails.svelte";
+  import WhatsappIcon from "./icons/whatsapp.svg";
+  import CloseIcon from "./icons/close.svg";
 
-  export let name;
   export let products;
+
+  let expanded = true;
+  function toggleExpanded() {
+    expanded = !expanded;
+  }
 </script>
 
 <main>
-  {#each products as product}
-    <Product {...product} />
-  {/each}
+  <div class="top">
+    <div class="summary">
+      <div class="left">
+        <Total />
+      </div>
+      <div class="right">
+        <button class="expand" on:click={toggleExpanded}
+          >{#if expanded}<CloseIcon />{:else}<WhatsappIcon />{/if}</button
+        >
+      </div>
+    </div>
 
-  <hr />
-  <Total />
+    {#if expanded}
+      <div class="details" transition:slide>
+        <OrderDetails />
+      </div>
+    {/if}
+  </div>
 
-  <hr />
-  <OrderDetails />
+  <div class="bottom">
+    {#each products as product}
+      <Product {...product} />
+    {/each}
+  </div>
 </main>
+
+<style>
+  :global(:root) {
+    --header-color: #5800ff;
+  }
+
+  main {
+  }
+
+  .top {
+    background: var(--header-color);
+    color: #fff;
+    position: sticky;
+    top: 0;
+  }
+
+  .summary {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 10px;
+  }
+
+  .left {
+    flex-grow: 1;
+  }
+
+  .right {
+  }
+
+  .details {
+    padding: 10px;
+  }
+
+  button.expand {
+    background: transparent;
+    border: 1px solid #fff;
+    border-radius: 5px;
+    color: #fff;
+    width: 36px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px;
+    margin: 0;
+  }
+
+  button.expand :global(svg) {
+    width: 100%;
+    height: 100%;
+  }
+
+  .bottom {
+  }
+</style>
--- src/OrderDetails.svelte
+++ src/OrderDetails.svelte
@@ -1,6 +1,7 @@
 <script>
   import { order } from "./stores";
   import { orderTotals } from "./stores";
+  import WhatsappIcon from "./icons/whatsapp.svg";
 
   function send() {
     const phoneNumber = `54${$order.phone}`;
@@ -22,17 +23,49 @@
 
 <div>
   <label>
-    Teléfono:
+    Enviar a
     <br />
     <input bind:value={$order.phone} type="tel" />
   </label>
 
   <br />
   <label>
-    Notas:
+    Notas
     <br />
     <textarea bind:value={$order.notes} />
   </label>
 
-  <button on:click={send}>Enviar pedido</button>
+  <button on:click={send}>Enviar pedido <WhatsappIcon /></button>
 </div>
+
+<style>
+  input,
+  textarea {
+    background: rgba(255, 255, 255, 0.1);
+    border: 1px solid #fafafa;
+    border-radius: 5px;
+    color: #fff;
+
+    margin: 5px 0;
+
+    width: 100%;
+  }
+
+  button {
+    background: var(--header-color);
+    border: 1px solid #fafafa;
+    border-radius: 5px;
+    color: #fff;
+    margin: 10px auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  button :global(svg) {
+    width: 24px;
+    height: 100%;
+    margin-left: 8px;
+  }
+</style>
